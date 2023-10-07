ce99661293423b46923498a455f61deca2eccbb8
tatsuya730 <t.taniguchi730@gmail>
2023-10-07 12:09:46 +0900

    メンバー一覧表示

---
+++ list_app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/members')
+def members():
+    members = ["Bob", "Tom", "Ken"]
+    return render_template("members.html", members=members)
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
